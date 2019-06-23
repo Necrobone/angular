9088b449cdac91a94e374c0f8f085a9edca8a168
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-06-23 12:16:13 +0200

    Person input event binding and local references

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
+import { PersonsInputComponent } from './persons/persons-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonsComponent,
+    PersonsInputComponent
   ],
   imports: [
     BrowserModule,
