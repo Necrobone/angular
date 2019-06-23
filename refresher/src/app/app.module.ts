9e30d893927d23096bc000f61e45b20047fda8bc
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-06-23 12:09:59 +0200

    Person component

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
