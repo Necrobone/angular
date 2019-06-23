1976972de798af8a09aee850ff5aa25f9160512d
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-06-23 12:26:59 +0200

    Custom events

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PersonsComponent } from './persons/persons.component';
-import { PersonsInputComponent } from './persons/persons-input.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PersonsComponent} from './persons/persons.component';
+import {PersonsInputComponent} from './persons/persons-input.component';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import { PersonsInputComponent } from './persons/persons-input.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- refresher/src/app/persons/persons-input.component.ts
+++ refresher/src/app/persons/persons-input.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-persons-input',
@@ -6,7 +6,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./persons-input.component.css']
 })
 export class PersonsInputComponent {
-  onCreatePerson(personName: string) {
-    console.log('Created a person: ' + personName);
+  @Output() personCreate = new EventEmitter<string>();
+  enteredPersonName = '';
+
+  onCreatePerson() {
+    console.log('Created a person: ' + this.enteredPersonName);
+    this.personCreate.emit(this.enteredPersonName);
+    this.enteredPersonName = '';
   }
 }
